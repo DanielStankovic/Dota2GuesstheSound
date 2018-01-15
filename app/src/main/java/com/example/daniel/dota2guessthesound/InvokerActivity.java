568dca100d9c65bb08a6507eeb231e06c7322436
Daniel Stankovic <danielstankovic1992@gmail.com>
2018-01-15 18:44:14 +0100

    InvokerMode done logic for sounds. Things to do: Timer and Gameover Screen

--- app/src/main/java/com/example/daniel/dota2guessthesound/InvokerActivity.java
+++ app/src/main/java/com/example/daniel/dota2guessthesound/InvokerActivity.java
@@ -1,10 +1,16 @@
 package com.example.daniel.dota2guessthesound;
 
 import android.content.pm.ActivityInfo;
+import android.graphics.Typeface;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.bumptech.glide.Glide;
@@ -12,6 +18,9 @@ import com.bumptech.glide.Glide;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class InvokerActivity extends AppCompatActivity {
 
@@ -20,17 +29,38 @@ public class InvokerActivity extends AppCompatActivity {
     ImageView buttonE;
     ImageView buttonInvoke;
 
+    ImageView heart1;
+    ImageView heart2;
+    ImageView heart3;
+
+    TextView timerTextView;
+    TextView welcomeTextView;
+
+    LinearLayout gameStartContainer;
+    LinearLayout playGameContainer;
+
+    MediaPlayer mediaPlayer;
+    int chosenSound;
+    Random random;
+
+    long playRate = 5000;
+    int soundRateCounter =1;
+    int numberOfHearts = 2;
+
+    Timer timer;
 
 
     private ArrayList<ImageView> orbs;
 
+    private ArrayList<String> soundOrder =  new ArrayList<>();
 
+    private ArrayList<Integer> sounds = new ArrayList<>(Arrays.asList(R.raw.invoker_mode_alacrity, R.raw.invoker_mode_chaos_meteor, R.raw.invoker_mode_cold_snap, R.raw.invoker_mode_deafening_blast, R.raw.invoker_mode_emp, R.raw.invoker_mode_forge_spirit, R.raw.invoker_mode_ghost_walk, R.raw.invoker_mode_ice_wall, R.raw.invoker_mode_sun_strike, R.raw.invoker_mode_tornado));
 
+    private ArrayList<String> names = new ArrayList<>(Arrays.asList("alacrity", "chaos meteor", "cold snap" ,"deafening blast", "emp", "forge spirit", "ghost walk", "ice wall", "sun strike", "tornado"));
 
     String spell;
 
     List<String> spells = new ArrayList<>(3);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,15 +70,44 @@ public class InvokerActivity extends AppCompatActivity {
         buttonQ =(ImageView) findViewById(R.id.buttonQ);
         buttonW = (ImageView)findViewById(R.id.buttonW);
         buttonE =(ImageView) findViewById(R.id.buttonE);
+        buttonInvoke = (ImageView) findViewById(R.id.buttonInvoke);
+
         ImageView   orb1 = (ImageView)findViewById(R.id.orb1);
         ImageView  orb2 = (ImageView)findViewById(R.id.orb2);
         ImageView   orb3 =(ImageView) findViewById(R.id.orb3);
-        buttonInvoke = (ImageView) findViewById(R.id.buttonInvoke);
+
+        heart1 = (ImageView)findViewById(R.id.heart1Image);
+        heart2 = (ImageView)findViewById(R.id.heart2Image);
+        heart3 = (ImageView)findViewById(R.id.heart3Image);
+
+        gameStartContainer = (LinearLayout)findViewById(R.id.gameStartContainer);
+        playGameContainer = (LinearLayout)findViewById(R.id.playGameContainer);
+
+        timerTextView = (TextView)findViewById(R.id.timerTextView);
+        welcomeTextView = (TextView)findViewById(R.id.welcomeTextView);
+
+        random = new Random();
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        setFonts();
+
         orbs = new ArrayList<>(Arrays.asList(orb1, orb2,orb3));
+
         for (int i = 0; i < 3 ; i++) {
             spells.add(null);
 
-        }
+                    }
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.invoker_mode_alacrity);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        timer.cancel();
+        timer.purge();
+
     }
 
     public void addQ (View view){
@@ -94,43 +153,63 @@ public class InvokerActivity extends AppCompatActivity {
 
             }
 
+
             if (quasOrbNumber == 3) {
-                spell = "Cold Snap";
+                spell = "cold snap";
             } else if (quasOrbNumber == 2 && wexOrbNumber == 1) {
 
-                spell = "Ghost Walk";
+                spell = "ghost walk";
 
             } else if (quasOrbNumber == 2 && exortOrbNumber == 1) {
 
-                spell = "Ice Wall";
+                spell = "ice wall";
 
             } else if (wexOrbNumber == 3) {
 
-                spell = "EMP";
+                spell = "emp";
 
             } else if (wexOrbNumber == 2 && quasOrbNumber == 1) {
 
-                spell = "Tornado";
+                spell = "tornado";
             } else if (wexOrbNumber == 2 && exortOrbNumber == 1) {
-                spell = "Alactrity";
+                spell = "alacrity";
             } else if (exortOrbNumber == 3) {
 
-                spell = "Sun Strike";
+                spell = "sun strike";
 
             } else if (exortOrbNumber == 2 && quasOrbNumber == 1) {
 
 
-                spell = "Forge Spirit";
+                spell = "forge spirit";
             } else if (exortOrbNumber == 2 && wexOrbNumber == 1) {
 
-                spell = "Chaos Meteor";
+                spell = "chaos meteor";
             } else if (quasOrbNumber == 1 && wexOrbNumber == 1 && exortOrbNumber == 1) {
 
-                spell = "Deafening Blast";
+                spell = "deafening blast";
 
             }
 
 
+            if (soundOrder.get(soundOrder.size() - 1).equals(spell)) {
+
+                Toast.makeText(getApplicationContext(), "CORRECT", Toast.LENGTH_SHORT).show();
+            } else {
+                if (numberOfHearts > 0) {
+                    if(numberOfHearts == 2){
+                        numberOfHearts--;
+                        heart3.setVisibility(View.INVISIBLE);
+                    }else if(numberOfHearts == 1){
+                        numberOfHearts--;
+                        heart2.setVisibility(View.INVISIBLE);
+                    }
+
+            }
+            else{
+                    Toast.makeText(getApplicationContext(), "Game Over", Toast.LENGTH_SHORT).show();
+                 InvokerActivity.this.finish();
+                }
+      }
 
         }
 
@@ -163,5 +242,125 @@ public class InvokerActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+
+
+    private void setFonts(){
+
+        Typeface scoreFont = Typeface.createFromAsset(getAssets(),"fonts/score_font.ttf" );
+        timerTextView.setTypeface(scoreFont);
+        welcomeTextView.setTypeface(scoreFont);
+        timerTextView.setTypeface(scoreFont);
+    }
+
+    public void playSound(){
+
+        chosenSound = random.nextInt(sounds.size());
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), sounds.get(chosenSound));
+        mediaPlayer.start();
+        soundOrder.add(names.get(chosenSound));
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+
+                if(soundRateCounter == 5){
+                    Log.i("AAAAAA", " soundcounter je 5");
+                    timer.cancel();
+                    timer.purge();
+                    playRate = 6000;
+                    doSound(playRate);
+                }
+                if(soundRateCounter == 10){
+                    Log.i("AAAAAA", " soundcounter je 10");
+                    timer.cancel();
+                    timer.purge();
+                    playRate = 5000;
+                    doSound(playRate);
+
+                }
+
+                if(soundRateCounter == 15){
+                    Log.i("AAAAAA", " soundcounter je 15");
+                    timer.cancel();
+                    timer.purge();
+                    playRate = 4000;
+                    doSound(playRate);
+
+                }
+
+                if(soundRateCounter == 20){
+                    Log.i("AAAAAA", " soundcounter je 20");
+                    timer.cancel();
+                    timer.purge();
+                    playRate = 3000;
+                    doSound(playRate);
+
+                }
+
+                if(soundRateCounter == 25){
+                    Log.i("AAAAAA", " soundcounter je 25");
+                    timer.cancel();
+                    timer.purge();
+                    playRate = 2000;
+                    doSound(playRate);
+
+                }
+
+                if(soundRateCounter == 30){
+                    Log.i("AAAAAA", " soundcounter je 30");
+                    timer.cancel();
+                    timer.purge();
+                    playRate = 1000;
+                    doSound(playRate);
+
+                }
+
+                if(soundRateCounter == 35){
+                    Log.i("AAAAAA", " soundcounter je 35");
+                    timer.cancel();
+                    timer.purge();
+                    playRate = 500;
+                    doSound(playRate);
+
+                }
+                if(soundRateCounter == 40){
+                    Log.i("AAAAAA", " soundcounter je 40");
+                    timer.cancel();
+                    timer.purge();
+                    playRate = 250;
+                    doSound(playRate);
+
+                }
+
+                soundRateCounter++;
+
+            }
+        });
+
+    }
+
+    public void doSound(long playRate){
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if (!mediaPlayer.isPlaying()) {
+                    mediaPlayer.release();
+                    playSound();
+                    if(numberOfHearts < 0 ) {
+                        this.cancel();
+                    }
+                }
+            }
+        }, 0, playRate);
+
+    }
+
+    public void goInvoker(View view){
+        gameStartContainer.setVisibility(View.GONE);
+        playGameContainer.setVisibility(View.VISIBLE);
+        doSound(playRate);
+
     }
 }
